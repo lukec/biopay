d53b49e7d8a705881924c90195bd65451f9af2ef
Luke Closs <luke@5thplane.com>
2011-07-07 21:27:18 -0700

    redo the look of the site, add TOS, privacy, ...

--- lib/biopay.pm
+++ lib/biopay.pm
@@ -7,12 +7,4 @@ get '/' => sub {
     template 'index';
 };
 
-get '/member/signup' => sub {
-    template 'signup';
-};
-
-post '/member/signup' => sub {
-    template 'signup_complete';
-};
-
 true;
