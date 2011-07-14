167d2fbec64f52b4316485585c3a59b935d1d28f
Luke Closs <luke@5thplane.com>
2011-07-14 13:18:48 -0700

    more DWIMyness on the jobs

--- lib/Biopay/Command.pm
+++ lib/Biopay/Command.pm
@@ -3,12 +3,15 @@ use Moose;
 use methods;
 use Dancer::Plugin::CouchDB;
 use Dancer ':syntax';
+use Biopay::Member;
 
 extends 'Biopay::Resource';
 
 has 'command' => (is => 'ro', isa => 'Str',       required => 1);
 has 'args'    => (is => 'ro', isa => 'HashRef[]', required => 1);
 has 'timestamp' => (is => 'ro', isa => 'Int',     required => 1);
+
+has 'member'  => (is => 'ro', isa => 'Maybe[Object]', lazy_build => 1);
 
 sub Create {
     my $class = shift;
@@ -20,4 +23,17 @@ sub Create {
             %args,
         },
     )->recv;
+}
+
+sub NextJob {
+    my $class = shift;
+    my $result = $couch->view('jobs/all')->recv;
+    return undef unless @{ $result->{rows} };
+    return $class->new( $result->{rows}[0]{value} );
+}
+
+method _build_member {
+    my $id = $self->args->{member_id};
+    return undef unless $id;
+    return Biopay::Member->By_id($id);
 }
