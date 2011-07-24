05aa56982961c3747c4f64c30fa20efbd66999ae
Luke Closs <luke@5thplane.com>
2011-07-23 22:24:12 -0700

    Update how the cardlock transactions are stored.

--- lib/Biopay/Transaction.pm
+++ lib/Biopay/Transaction.pm
@@ -7,15 +7,16 @@ use Biopay::Member;
 
 extends 'Biopay::Resource';
 
-has 'epoch_time' => (isa => 'Num', is => 'ro', required => 1);
-has 'date' => (isa => 'Str', is => 'ro', required => 1);
-has 'price_per_litre' => (isa => 'Num', is => 'ro', required => 1);
-has 'txn_id' => (isa => 'Num', is => 'ro', required => 1);
-has 'paid' => (isa => 'Bool', is => 'rw', required => 1);
-has 'litres' => (isa => 'Num', is => 'ro', required => 1);
-has 'member_id' => (isa => 'Num', is => 'ro', required => 1);
-has 'price' => (isa => 'Num', is => 'ro', required => 1);
-has 'pump' => (isa => 'Str', is => 'ro', required => 1);
+has 'epoch_time'      => (isa => 'Num',  is => 'ro', required => 1);
+has 'date'            => (isa => 'Str',  is => 'ro', required => 1);
+has 'price_per_litre' => (isa => 'Num',  is => 'ro', required => 1);
+has 'txn_id'          => (isa => 'Str',  is => 'ro', required => 1);
+has 'cardlock_txn_id' => (isa => 'Num',  is => 'ro');
+has 'paid'            => (isa => 'Bool', is => 'rw', required => 1);
+has 'litres'          => (isa => 'Num',  is => 'ro', required => 1);
+has 'member_id'       => (isa => 'Num',  is => 'ro', required => 1);
+has 'price'           => (isa => 'Num',  is => 'ro', required => 1);
+has 'pump'            => (isa => 'Str',  is => 'ro', required => 1);
 
 with 'Biopay::Roles::HasMember';
 
